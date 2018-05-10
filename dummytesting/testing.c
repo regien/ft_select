97839722c6f0673d3045444568c0055d5d73499a
gerardo <gerardomalpartidavela@gmail.com>
2018-05-10 16:38:11 -0700

    playing around

--- dummytesting/testing.c
+++ dummytesting/testing.c
@@ -5,10 +5,39 @@
 #include <sys/ioctl.h>
 
 
-#include <termios.h>
+#include <curses.h>
+//#include <termios.h>
 #include <unistd.h>
 #include <term.h>
 
+/*
+int		vaina(void)
+{
+	char			buffer[3];
+	int				pendejada;
+
+	pendejada = 1;
+	while (pendejada)
+	{
+		read(0, buffer, 3);
+		printf("readding = |%s|", buffer);
+		if (buffer[0] == 27)
+			printf("esto es una flecha\n");
+//		else if (buffer[0] == 4)
+		else if (buffer[0] == 4)
+		{
+			printf("Ctlr+d, STOP !\n");
+			return (0);
+		}
+	}
+}
+*/
+
+int	putpendejada(int c)
+{
+	printf("%c", c);
+	return (1);
+}
 
 int		main(void)
 {
@@ -17,25 +46,24 @@ int		main(void)
 
 	if (!(name_term = getenv("TERM")))
 		return (-1);
-	if (tgetent(NULL, &name_term) == ERR)
+	if (tgetent(NULL, name_term) == ERR)
 		return (-1);
-	if (tcgetattr(0, term) == -1)
+	if (tcgetattr(0, &term) == -1)
 		return (-1);
 
-	vaina();
+//	vaina();
+	char			*res;
+
+	if (!(res = tgetstr("cl", NULL)))
+		return (-1);
+	tputs(res, 0, putpendejada);
 	return (0);
 }
 
-int		vaina(void)
-{
-	char			buffer[3];
-
-	while (1)
-	{
-		read(0, buffer, 3);
-		if (buffer[0] == 27)
-	}
-}
+/*
+** they are just a series of commands that outputs a special string
+** that makes the terminal executes an action
+*/
 
 
 /*
